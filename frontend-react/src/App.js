61061969fa273c620388814de2a8b443a65b44ac
a7med-rashad <a7medra4aad3@gmail.com>
2022-11-13 07:57:48 +0200

    new edit

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,7 +5,7 @@ import './App.scss'
 const App = () => {
   return (
     <div className='app'>
-      <Navbar />
+      {/* <Navbar /> */}
       <About />
       <Footer />
       <Header />
--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -34,6 +34,7 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import me from '../assets/me.png';
 
 export default {
   email,
@@ -69,4 +70,5 @@ export default {
   nb,
   skype,
   spotify,
+  me,
 };
